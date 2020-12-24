1f002ab204977891cb47a66b82e071282c52c234
Salvo Cavallaro <salvator.cavallaro@gmail.com>
2020-12-24 10:54:07 +0100

    First Spring commit

---
+++ src/test/java/dsbd/project/monolithic_order/MonolithicOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package dsbd.project.monolithic_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithicOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
