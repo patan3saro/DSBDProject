51566338da4f9e6a81f76a4a9717448805518673
Salvo Cavallaro <salvator.cavallaro@gmail.com>
2021-01-05 09:41:36 +0100

    Modify

--- ecommerce/ordermanager/src/main/java/dsbd/project/ordermanager/controller/OrderController.java
+++ ecommerce/ordermanager/src/main/java/dsbd/project/ordermanager/controller/OrderController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/main
 import java.util.Optional;
 
 @Controller
